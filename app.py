c7c84653730cc19a52c0281b8f6d78b6287390c8
JJ Ferman <fermanjj@gmail.com>
2017-07-16 14:32:37 -0700

    Spelling corrections and added some doc strings.

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+"""
+Author: JJ Ferman (fermanjj@gmail.com)
+
+This is a few endpoints implemented in the
+Flask web framework to receive PNRs and
+determine price drops on similar or exact
+flights.
+
+"""
 from flask import (
     Flask, jsonify
 )
@@ -8,10 +17,17 @@ app = Flask(__name__)
 app.secret_key = APP_SECRET
 
 
-@app.route('/prns/')
-def prns():
-    return jsonify({})
+@app.route('/pnrs/<string:pnr>')
+def pnrs(pnr):
+    """Accepts a GET requests with a string
+    of a PNR.
+    Currently just returns the passed in PNR.
+
+    :param str pnr: Passenger Name Record
+    :return: pnr
+    """
+    return jsonify(pnr)
 
 
 if __name__ == '__main__':
-    app(debug=DEBUG, port=PORT)
+    app.run(debug=DEBUG, port=PORT)
